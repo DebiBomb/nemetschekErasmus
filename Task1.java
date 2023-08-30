58688efc8a48f58527f4af0fc1640f5aa323839b
DebiBomb <matteodebia05@gmail.com>
2023-08-30 10:47:16 +0300

    New task1, but not right at all

--- Task1.java
+++ Task1.java
@@ -2,16 +2,33 @@ package nemetschekErasmus;
 public class Task1 {
     public static void main(String[] args){
 
-        System.out.println(Encoded(""));
+        System.out.println(Encoded1("VVRBRB"));
+        System.out.println(Encoded2("VVRBRB"));
     }
 
-    static public String Encoded(String toEncoded){
+    static public String Encoded1(String toEncoded){
         
         String toDecoded = "";
         int n=1;
 
         for(int i=toEncoded.length()-1; i>0; i--){
             if(toEncoded.charAt(i) == toEncoded.charAt(i-1)){
+                n++;
+            }else{
+                toDecoded += String.valueOf(n) + toEncoded.charAt(i);
+                n=1;
+            }
+        }
+        return toDecoded;
+    }
+
+    static public String Encoded2(String toEncoded){
+        
+        String toDecoded = "";
+        int n=1;
+
+        for(int i=0; i<toEncoded.length()-1; i++){
+            if(toEncoded.charAt(i) == toEncoded.charAt(i+1)){
                 n++;
             }else{
                 toDecoded += String.valueOf(n) + toEncoded.charAt(i);
