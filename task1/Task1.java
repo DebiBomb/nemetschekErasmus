46103e4418a83804a1bcd902c552377213eae5c1
DebiBomb <matteodebia05@gmail.com>
2023-08-31 11:13:02 +0300

    Just for a test
    Just commit the folder task1 for take confidence with git.

---
+++ task1/Task1.java
@@ -0,0 +1,69 @@
+package nemetschekErasmus;
+public class Task1 {
+    public static void main(String[] args){
+
+        System.out.println(Encoded1("AAAAAVVSSA"));
+        System.out.println(Encoded2("AAAAAVVSSA"));
+        System.out.println(Encoded3("AAAAAVVSSA"));
+    }
+
+    static public String Encoded1(String toEncoded){
+        
+        String toDecoded = "";
+        int n=1;
+
+        for(int i=toEncoded.length()-1; i>0; i--){
+            if(toEncoded.charAt(i) == toEncoded.charAt(i-1)){
+                n++;
+            }else{
+                toDecoded += String.valueOf(n) + toEncoded.charAt(i);
+                n=1;
+            }
+        }
+        return toDecoded;
+    }
+
+    static public String Encoded2(String toEncoded){
+        
+        String toDecoded = "";
+        int n=1;
+
+        for(int i=0; i<toEncoded.length()-1; i++){
+            if(toEncoded.charAt(i) == toEncoded.charAt(i+1)){
+                n++;
+            }else{
+                toDecoded += String.valueOf(n) + toEncoded.charAt(i);
+                n=1;
+            }
+        }
+        return toDecoded;
+    }
+
+    static public String Encoded3(String toEncoded){
+
+        String toDecoded = "";
+        int counter = 1;
+        char a = toEncoded.charAt(0);
+        char currentLetter;
+
+        for(int i=1; i<toEncoded.length(); i++){
+            currentLetter = toEncoded.charAt(i);
+            
+         
+           
+
+
+            if(currentLetter == a)
+                counter++;
+            else{
+
+                toDecoded += String.valueOf(counter) + a;
+                counter =1 ;
+                a = currentLetter;
+            }                
+        }
+        toDecoded += String.valueOf(counter) + toEncoded.charAt(toEncoded.length()-1); ;
+        return toDecoded;
+    }
+  
+}
